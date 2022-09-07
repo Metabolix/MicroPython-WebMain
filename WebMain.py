945c0877c5085abfe2fc5a0762910456b0c5f7a9
Lauri Kenttä <lauri.kentta@gmail.com>
2022-09-07 09:12:37 +0300

    Add WebRequest class for parsing and replying requests.

--- WebMain.py
+++ WebMain.py
@@ -1,7 +1,34 @@
 """Web-based main loop for MicroPython. See Example.py for instructions."""
 
-import socket, time, machine, sys
+import socket, time, machine, sys, os, gc
 from Timeout import Timeout
+
+class WebRequest:
+    def __init__(self, socket):
+        self.socket = socket
+        self.output_started = False
+
+    def parse(self):
+        self.socket.setblocking(True)
+        head = bytearray()
+        while True:
+            s = self.socket.readline()
+            if not s or not s.strip():
+                break
+            head.extend(s)
+        head = head.strip()
+        raw_headers = head.decode().split("\r\n")
+        self.method, self.uri, self.http_version = raw_headers[0].split(" ")
+
+    def reply(self, content = b"", status = 200, mime = b"text/plain; charset=UTF-8"):
+        if not self.output_started:
+            status = str(status).encode()
+            if type(mime) == str:
+                mime = mime.encode()
+            self.socket.send(b"HTTP/1.0 " + status + b" -\r\nContent-Type: " + mime + b"\r\n\r\n")
+            self.output_started = True
+        if content:
+            self.socket.send(content)
 
 class WebMain:
     @classmethod
@@ -27,15 +54,16 @@ class WebMain:
         if retry_acceptable and retry_acceptable.expired():
             machine.reset()
 
-    def __init__(self, network):
+    def __init__(self, network, display_errors = True):
         self.network = network
         self.socket = None
+        self.display_errors = display_errors
 
     def _run(self):
         while True:
             if self.network.connected() and not self.socket:
                 self._listen()
-            if not self.accept_request():
+            if not self._accept_request():
                 time.sleep_ms(10)
 
     def _listen(self):
@@ -46,19 +74,47 @@ class WebMain:
         self.socket.bind(socket.getaddrinfo("0.0.0.0", 80)[0][-1])
         self.socket.listen(4)
 
-    def accept_request(self):
+    def _accept_request(self):
         try:
             client, client_address = self.socket.accept()
         except:
             return
 
         try:
-            client.setblocking(True)
-            while client.readline().strip():
-                continue
-            client.send(b"HTTP/1.0 404 Not Found\r\n\r\nThis server has nothing to offer.")
+            error = (401, b"Failed to parse request")
+            request = WebRequest(client)
+            request.parse()
+            print(f"{request.method} {request.uri}")
+            error = (500, b"Failed to process request")
+            self._dispatch_request(request)
+            if not request.output_started:
+                request.reply(status = 404, content = b"Not found")
         except BaseException as e:
             sys.print_exception(e)
+            try:
+                if not request.output_started:
+                    request.reply(status = error[0], content = error[1])
+                if self.display_errors:
+                    request.reply(b"\n\n")
+                    sys.print_exception(e, client)
+            except:
+                pass
         finally:
             client.close()
         return True
+
+    def _dispatch_request(self, request):
+        if request.uri != "/":
+            return
+        gc.collect()
+        uname = os.uname()
+        request.reply(status = 200, mime = b"text/html; charset=UTF-8")
+        request.reply(f"""<!DOCTYPE html>
+            <title>WebMain on {uname.nodename}</title>
+            <h1>WebMain on {uname.nodename}</h1>
+            <p>machine: {uname.machine}</p>
+            <p>version: {uname.version}</p>
+            <p>reset_cause: {machine.reset_cause()}</p>
+            <p>freq: {machine.freq()}</p>
+            <p>mem: {gc.mem_alloc()} used, {gc.mem_free()} free</p>
+        """)
