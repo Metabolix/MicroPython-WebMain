6c8890b16d67de7f026ea82aa84157ec0bbc06bf
Lauri Kenttä <lauri.kentta@gmail.com>
2022-11-09 21:59:25 +0200

    Example: reboot if network is broken.

--- Example.py
+++ Example.py
@@ -54,6 +54,16 @@ class ExampleMain(WebMain):
         network = SimpleWLAN.from_config()
         super().__init__(network)
 
+        # Fail-safe: reboot if the network seems broken (ENOMEM in ping).
+        def reboot_if_network_is_broken(request):
+            if network.is_broken():
+                self._log("Network is broken.")
+                import machine
+                machine.reset()
+            else:
+                request and request.reply("Not broken.")
+        self.add_module(reboot_if_network_is_broken)
+
         # Add any common static content (files or dirs).
         self.add_static("/favicon.ico")
         self.add_static("/static-pages")
